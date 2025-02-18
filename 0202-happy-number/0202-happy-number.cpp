4ed6b8fd982635c1a4feffa4f64eccd83a513dff
Yash <yashrathee0707@gmail.com>
2025-02-18 21:27:59 +0530

    Time: 0 ms (100%), Space: 8.4 MB (38.26%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,27 @@
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+        
+        while (n != 1) {
+            if (seen.count(n)) {
+                return false;
+            }
+            seen.insert(n);
+            
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            
+            n = sum;
+        }
+        
+        return true;
+    }
+};
