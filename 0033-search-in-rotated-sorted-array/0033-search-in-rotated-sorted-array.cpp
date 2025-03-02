987f0686a9a099235bdafdcb40ada75e5a8e1e1e
Yash <yashrathee0707@gmail.com>
2025-03-02 15:26:57 +0530

    Time: 0 ms (100%), Space: 15.1 MB (94.83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
+    int search(vector<int>& nums, int target) {  // Function to search for target in rotated sorted array
+        int start = 0;  // Initialize start pointer
+        int end = nums.size() - 1;  // Initialize end pointer
 
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(nums[mid] == target){
+        while(start <= end){  // Binary search loop
+            int mid = start + (end - start)/2;  // Calculate mid to avoid integer overflow
+            if(nums[mid] == target){  // If mid element is target, return its index
                 return mid;
             }
-            if(nums[start] <= nums[mid]){
-                if(nums[start] <= target && target < nums[mid]){
-                    end = mid - 1;
+            if(nums[start] <= nums[mid]){  // If left half is sorted
+                if(nums[start] <= target && target < nums[mid]){  // If target is within sorted left half
+                    end = mid - 1;  // Move left
                 }
-                else{
-                    start = mid + 1;
+                else{  
+                    start = mid + 1;  // Otherwise, move right
                 }
-            }else{
-                if(nums[end] >= target && target > nums[mid]){
-                    start = mid + 1;
+            }else{  // If right half is sorted
+                if(nums[end] >= target && target > nums[mid]){  // If target is within sorted right half
+                    start = mid + 1;  // Move right
                 }
-                else{
-                    end = mid - 1;
+                else{  
+                    end = mid - 1;  // Otherwise, move left
                 }
             }
         }
-        return -1;
+        return -1;  // Target not found
     }
 };
